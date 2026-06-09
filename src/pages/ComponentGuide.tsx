67115cca47b367fcb56002c6f7fedc1b5446c9c7
FarahKhatab <f.alkhatab@qbs.jo>
2026-06-09 15:18:33 +0300

    test deployement

--- src/pages/ComponentGuide.tsx
+++ src/pages/ComponentGuide.tsx
@@ -27,7 +27,7 @@ import {
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbList,
-  BreadcrumbPage,
+  BreadcrumbPage, 
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
 import { Button } from "@/components/ui/button"
